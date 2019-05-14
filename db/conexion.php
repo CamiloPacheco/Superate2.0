ad49e80476bbf00c1e5968d60d84b61e600baf2e
CamiloPacheco <cacomas@uninorte.edu.co>
2019-05-14 10:39:34 -0500

    mi segundo commit

--- db/conexion.php
+++ db/conexion.php
@@ -1,13 +1,13 @@
-<?php 
-class database  
+<?php
+class database
 {
     public static  function conectar()
     {
-        $db= new mysqli("localhost","root","","superatedatabase");
+        $db= new mysqli("remotemysql.com","lOWXmNRh2e","0ukDBh0PER","lOWXmNRh2e");
         $r=$db->query("set names 'utf8' ");
-        return $db;    
+        return $db;
     }
-   
+
 }
 
 
